75f832a36ecf02089f9bb6f24d14476e6b298590
Ivan Iskandar <iiiiskandar14@gmail.com>
2025-06-20 21:09:10 +0700

    16kb

--- sqlite-android/src/main/jni/Application.mk
+++ sqlite-android/src/main/jni/Application.mk
@@ -2,5 +2,6 @@ APP_STL:=none
 APP_OPTIM := release
 APP_ABI := armeabi-v7a,arm64-v8a,x86,x86_64
 APP_CFLAGS += -Ofast
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 NDK_TOOLCHAIN_VERSION := clang
 NDK_APP_LIBS_OUT=../jniLibs
