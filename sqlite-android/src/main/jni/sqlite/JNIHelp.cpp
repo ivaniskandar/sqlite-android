9b9404c2b1efef26dd1b80c20be5ccc1b3315b26
Ivan Iskandar <iiiiskandar14@gmail.com>
2026-02-07 12:51:59 +0700

    fix build
    used gemini so not entirely sure if it's correct. but it builds.

--- sqlite-android/src/main/jni/sqlite/JNIHelp.cpp
+++ sqlite-android/src/main/jni/sqlite/JNIHelp.cpp
@@ -194,21 +194,23 @@ int jniThrowIOException(C_JNIEnv* env, int errnum) {
     return jniThrowException(env, "java/io/IOException", message);
 }
 
-const char* jniStrError(int errnum, char* buf, size_t buflen) {
-#if __GLIBC__
-    // Note: glibc has a nonstandard strerror_r that returns char* rather than POSIX's int.
-    // char *strerror_r(int errnum, char *buf, size_t n);
-    return strerror_r(errnum, buf, buflen);
-#else
-    int rc = strerror_r(errnum, buf, buflen);
-    if (rc != 0) {
-        // (POSIX only guarantees a value other than 0. The safest
-        // way to implement this function is to use C++ and overload on the
-        // type of strerror_r to accurately distinguish GNU from POSIX.)
+// A templated helper function that calls the right strerror_r.
+template <typename T>
+static inline const char* strerror_r_helper(T strerror_r_ptr, int errnum, char* buf, size_t buflen) {
+    // This overload will be chosen for the int-returning POSIX version.
+    if (strerror_r_ptr(errnum, buf, buflen) != 0) {
         snprintf(buf, buflen, "errno %d", errnum);
     }
     return buf;
-#endif
+}
+
+static inline const char* strerror_r_helper(char* (*strerror_r_ptr)(int, char*, size_t), int errnum, char* buf, size_t buflen) {
+    // This overload will be chosen for the char*-returning GNU version.
+    return strerror_r_ptr(errnum, buf, buflen);
+}
+
+const char* jniStrError(int errnum, char* buf, size_t buflen) {
+    return strerror_r_helper(&strerror_r, errnum, buf, buflen);
 }
 
 void* operator new (size_t size)        { return malloc(size); }
